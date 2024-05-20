e64ba07672c55c19113f9cc841324f75f67cf428
Hipsteryoda <ksmith952913@gmail.com>
2024-05-19 19:56:11 -0700

    fix: catch InputMismatchException handling for bet amount

--- FinalProject/src/main/java/com/testFinalProject/app/DoubleDice.java
+++ FinalProject/src/main/java/com/testFinalProject/app/DoubleDice.java
@@ -1,6 +1,7 @@
 package com.testFinalProject.app;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class DoubleDice {
   public static void main(String[] args) {
@@ -14,36 +15,45 @@ public class DoubleDice {
 
     
     while (currentMoney > 0) {
-      // Initial prompt and reading currentBet
+       // Initial prompt and reading currentBet
       System.out.println("You have $" + currentMoney);
       System.out.print("How much would you like to bet (Enter 0 to quit)? ");
-      currentBet = scnr.nextInt();    // FIXME: don't let user bet more than remaining money
-                                      // TODO: if user enters 0, print "See you around, winner!"
-                                      // TODO: allow user to bet in dollars and cents 
-                                      // TODO: don't allow the user to bet negative values
-                                      // TODO: don't crash if the user enters a non-numeric value
+      try {
+        currentBet = scnr.nextInt();    // FIXME: don't let user bet more than remaining money
+                                        // TODO: if user enters 0, print "See you around, winner!"
+                                        // TODO: allow user to bet in dollars and cents 
+                                          // TODO: don't allow the user to bet negative values
+                                        // TODO: don't crash if the user enters a non-numeric value
 
-      // Roll the dice
-      die1.roll();
-      die2.roll();
-      
-      // tell user what they rolled
-      System.out.println("You rolled a " + die1.toString() + " and a " + die2.toString());
+        // Roll the dice
+        die1.roll();                    // FIXME: the die values can come in as none. "You rolled a none and a four"
+        die2.roll();
+        
+        // tell user what they rolled
+        System.out.println("You rolled a " + die1.toString() + " and a " + die2.toString());
 
-      // Check the value of the dice
-      if (die1.equals(die2)) {
-        currentMoney += currentBet;
-        System.out.println("You win $" + currentBet);
-        System.out.println("");
+        // Check the value of the dice
+        if (die1.equals(die2)) {
+          currentMoney += currentBet;
+          System.out.println("You win $" + currentBet);
+          System.out.println("");
+        }
+        else {
+          // System.out.println("Die 1: " + die1.getValue() + " Die 2: " + die2.getValue());
+          currentMoney -= currentBet;
+          System.out.println("You lose $" + currentBet);
+          System.out.println("");
+        }
       }
-      else {
-        // System.out.println("Die 1: " + die1.getValue() + " Die 2: " + die2.getValue());
-        currentMoney -= currentBet;
-        System.out.println("You lose $" + currentBet);
-        System.out.println("");
+      catch (InputMismatchException e) {
+        System.out.println("Please enter a valid bet amount");
+        scnr.nextLine();      // FIXME: throwaway
+        System.out.println();
       }
     }
+
     System.out.println("You are out of money!");
     System.out.println("Better luck next time");
+
   }
 }
