e4ad4ae5b43fe1a9b9ac2cf36c5e8f0be0598648
Hipsteryoda <ksmith952913@gmail.com>
2024-05-19 20:27:18 -0700

    fix: allow user to exit when entering 0

--- FinalProject/src/main/java/com/testFinalProject/app/DoubleDice.java
+++ FinalProject/src/main/java/com/testFinalProject/app/DoubleDice.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class DoubleDice {
+  public static void quitMessage() {
+    System.out.println("See you around, winner!");
+  }
+
+  public static void outOfMoneyMessage() {
+    System.out.println("You are out of money!");
+    System.out.println("Better luck next time");
+  }
+
   public static void main(String[] args) {
     // instantiate a Scanner object, and two Die objects
     Scanner scnr = new Scanner(System.in);
@@ -12,9 +21,10 @@ public class DoubleDice {
     // initialize currentMoney and declare currentBet 
     int currentMoney = 100;
     int currentBet;
+    boolean quit = false;
 
     
-    while (currentMoney > 0) {
+    while ( (currentMoney > 0) || (!quit) ) {
        // Initial prompt and reading currentBet
       System.out.println("You have $" + currentMoney);
       System.out.print("How much would you like to bet (Enter 0 to quit)? ");
@@ -22,9 +32,12 @@ public class DoubleDice {
         currentBet = scnr.nextInt();    // FIXME: don't let user bet more than remaining money
                                         // TODO: if user enters 0, print "See you around, winner!"
                                         // TODO: allow user to bet in dollars and cents 
-                                          // TODO: don't allow the user to bet negative values
-                                        // TODO: don't crash if the user enters a non-numeric value
-
+                                        // TODO: don't allow the user to bet negative values
+        if (currentBet == 0) {
+          quit = true;
+          quitMessage();
+          break;
+        }
         // Roll the dice
         die1.roll();                    // FIXME: the die values can come in as none. "You rolled a none and a four"
         die2.roll();
@@ -51,9 +64,5 @@ public class DoubleDice {
         System.out.println();
       }
     }
-
-    System.out.println("You are out of money!");
-    System.out.println("Better luck next time");
-
   }
 }
