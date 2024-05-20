6fedf6667dae6fb03097427b2541388a828a15b5
Hipsteryoda <ksmith952913@gmail.com>
2024-05-19 21:26:52 -0700

    fix: message methods and readme details

--- FinalProject/src/main/java/com/testFinalProject/app/DoubleDice.java
+++ FinalProject/src/main/java/com/testFinalProject/app/DoubleDice.java
@@ -1,6 +1,7 @@
 package com.testFinalProject.app;
 
 import java.util.Scanner;
+import java.awt.Panel;
 import java.util.InputMismatchException;
 
 public class DoubleDice {
@@ -11,6 +12,46 @@ public class DoubleDice {
   public static void outOfMoneyMessage() {
     System.out.println("You are out of money!");
     System.out.println("Better luck next time");
+  }
+
+  public static void invalidInputMessage(Scanner scnr) {
+    System.out.println("Make sure your bet is a number. Please enter a valid bet amount.");
+    scnr.nextLine();
+    System.out.println();
+  }
+
+  public static void negativeBetMessage(Scanner scnr) {
+    System.out.println("You know negative bets aren't real, right? Please enter a valid bet amount.");
+    scnr.nextLine();
+    System.out.println();
+  }
+
+  public static void betTooBigMessage(Scanner scnr) {
+    System.out.println("Whoa there high roller, your bet is too large. Please enter a valid bet amount.");
+    scnr.nextLine();
+    System.out.println();
+  }
+
+  public static int playHand(int bet, int money, Die die1, Die die2) {
+    die1.roll();                    // FIXME: the die values can come in as none. "You rolled a none and a four"
+    die2.roll();
+    
+    // tell user what they rolled
+    System.out.println("You rolled a " + die1.toString() + " and a " + die2.toString());
+
+    // Check the value of the dice
+    if (die1.equals(die2)) {
+      money += bet;
+      System.out.println("You win $" + bet);
+      System.out.println("");
+    }
+    else {
+      // System.out.println("Die 1: " + die1.getValue() + " Die 2: " + die2.getValue());
+      money -= bet;
+      System.out.println("You lose $" + bet);
+      System.out.println("");
+    }
+    return money;
   }
 
   public static void main(String[] args) {
@@ -30,38 +71,24 @@ public class DoubleDice {
       System.out.print("How much would you like to bet (Enter 0 to quit)? ");
       try {
         currentBet = scnr.nextInt();    // FIXME: don't let user bet more than remaining money
-                                        // TODO: if user enters 0, print "See you around, winner!"
                                         // TODO: allow user to bet in dollars and cents 
-                                        // TODO: don't allow the user to bet negative values
-        if (currentBet == 0) {
+        if (currentBet == 0) {  // exit the program
           quit = true;
           quitMessage();
           break;
         }
-        // Roll the dice
-        die1.roll();                    // FIXME: the die values can come in as none. "You rolled a none and a four"
-        die2.roll();
-        
-        // tell user what they rolled
-        System.out.println("You rolled a " + die1.toString() + " and a " + die2.toString());
-
-        // Check the value of the dice
-        if (die1.equals(die2)) {
-          currentMoney += currentBet;
-          System.out.println("You win $" + currentBet);
-          System.out.println("");
+        else if (currentBet > currentMoney) { // check the user is not betting more than they have
+          betTooBigMessage(scnr);
         }
-        else {
-          // System.out.println("Die 1: " + die1.getValue() + " Die 2: " + die2.getValue());
-          currentMoney -= currentBet;
-          System.out.println("You lose $" + currentBet);
-          System.out.println("");
+        else if (currentBet < 0) {
+          negativeBetMessage(scnr);
+        }
+        else if(currentBet > 1) {
+          currentMoney = playHand(currentBet, currentMoney, die1, die2);
         }
       }
       catch (InputMismatchException e) {
-        System.out.println("Please enter a valid bet amount");
-        scnr.nextLine();      // FIXME: throwaway
-        System.out.println();
+        invalidInputMessage(scnr);
       }
     }
   }
